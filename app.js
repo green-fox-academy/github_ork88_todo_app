345c793d4007fbb88f354812b511ee4349f8bada
ork88 <ynekro@gmail.com>
2021-11-25 23:41:12 +0100

    aircraft

---
+++ app.js
@@ -0,0 +1,12 @@
+
+
+console.log ( 
+    `Parancssori Todo applikáció /n
+ ============================= 
+    
+ Parancssori argumentumok:
+        -l   Kilistázza a feladatokat
+        -a   Új feladatot ad hozzá
+        -r   Eltávolít egy feladatot
+        -c   Teljesít egy feladatot`
+        );
